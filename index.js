847d60505819bd63d55fcc7c62967dc39b30408e
Pravin-Parkhi <pravinparkhi.pp@gmail.com>
2019-05-28 00:48:35 +0530

    add case converter for lowercase and uppercase

---
+++ index.js
@@ -0,0 +1,13 @@
+// ES5 syntax
+// module.exports = (name) => {
+//   return `Hi ${name}`
+// }
+ 
+// ES6 syntax
+export const uppercase = (string) => {
+  return string.toUpperCase()
+}
+
+export const lowercase = (string) => {
+  return string.toLowerCase()
+}
